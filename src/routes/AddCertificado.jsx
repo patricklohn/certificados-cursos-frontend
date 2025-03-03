fca58124125f08aeed6d10803ef1ffe6c5148b2c
patricklohn <patricklohn3@gmail.com>
2025-03-03 18:35:50 -0300

    criação do forms

--- src/routes/AddCertificado.jsx
+++ src/routes/AddCertificado.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import axios from "../axios-config.js"
+import {useState, useEffect} from 'react'
 
 const AddCertificado = () => {
   return (
-    <div>
-      <h1>teste1</h1>
+    <div className="add-certificado">
+      <h2>Registre um novo certificado:</h2>
+      <form action="">
+        <label>
+          <p>Título:</p>
+          <input type="text" placeholder="Coloque o nome do Curso." name="title" />
+        </label>
+        <label>
+          <p>Descrição:</p>
+          <textarea type="text" placeholder="Explique sobre o curso." name="description" />
+        </label>
+        <label>
+          <p>Foto:</p>
+          <input type="file" name="image" />
+        </label>
+        <input type="submit" className="btn" value="Enviar"/>
+      </form>
     </div>
   )
 }

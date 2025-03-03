667971bf3732cdc8b1f1c03a457da4be56b2e7a8
patricklohn <patricklohn3@gmail.com>
2025-03-03 07:54:49 -0300

    Configuração do reactRouter Dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 
+import Error from './routes/Error.jsx';
+import Home from './routes/Home.jsx';
+import AddCertificado from './routes/AddCertificado.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <Error/>,
+    children: [
+      {path: "/", element: <Home/>},
+      {path: "/add-certific", element: <AddCertificado/>},
+    ],
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
---
+++ src/routes/AddCertificado.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddCertificado = () => {
+  return (
+    <div>
+      <h1>teste1</h1>
+    </div>
+  )
+}
+
+export default AddCertificado

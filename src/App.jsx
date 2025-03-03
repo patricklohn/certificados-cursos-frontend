e418ee24d65c0eb3bceabd1b95ae7cd187a141cc
patricklohn <patricklohn3@gmail.com>
2025-03-03 18:09:10 -0300

    Criação de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
         <div className='App'>
-          <h1>Certificados</h1>
+          <Navbar/>
           <div className='container'>
             <Outlet/>
           </div>
+          <Footer/>
         </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <h2>
+            <Link to="/">Certificados</Link>
+        </h2>
+        <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/add-certific">Adicionar Certificado</Link>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar

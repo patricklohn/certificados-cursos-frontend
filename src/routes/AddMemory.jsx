ff4b9bc0a5dd21e1ba3e22f27a829b4cf4d22c2c
patricklohn <patricklohn3@gmail.com>
2025-03-04 23:04:38 -0300

    Criado forma para enviar comentarios para o banco

--- src/routes/AddMemory.jsx
+++ src/routes/AddMemory.jsx
@@ -10,6 +10,9 @@ const addMemory = () => {
   const [certificado, setCertificado] = useState(null);
   const [comments, setComments] = useState([]);
 
+  const [name, setName] = useState("");
+  const [text, setText] = useState("");
+
   const getCertificado = async () =>{
     try {
       const res = await axios.get(`/api/Memory/${id}`);
@@ -26,6 +29,29 @@ const addMemory = () => {
     } 
   }
 
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+
+    try {
+
+    const comment = {name, text};
+
+    const res = await axios.patch(`/api/Memory/${id}/comment/`, comment)
+
+    const lastComment = res.data.memorie.comments.pop();
+
+    setComments((comments) => [...comments, lastComment]);
+    setName("")
+    setText("")
+
+    toast.success(res.data.msg)
+      
+    } catch (error) {
+      console.log(error)
+      toast.error(error.response.data.msg)
+    }
+
+  }
   useEffect(()=>{
     getCertificado();
   },[])
@@ -36,19 +62,19 @@ const addMemory = () => {
 
   return (
     <div className='memory-page'>
-      <iframe  src={`${axios.defaults.baseURL}uploadsArquivos/${certificado.src}#zoom=FitH&toolbar=0`} alt={`${certificado.title}`}/>
+      <iframe  src={`${axios.defaults.baseURL}uploadsArquivos/${certificado.src}#zoom=67`} alt={`${certificado.title}`}/>
       <h2>{certificado.title}</h2>
       {certificado.description !== 'undefined' && (
         <p>{certificado.description}</p>
       )}
       <div className="comment-form">
         <h3>Envie o seu comentário:</h3>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>
-            <input type="text" placeholder='Seu nome...'/>
+            <input type="text" placeholder='Seu nome...' onChange={(e) => setName(e.target.value)} value={name}/>
           </label>
           <label>
-            <textarea placeholder='Sua anotação sobre o curso...'/>
+            <textarea placeholder='Sua anotação sobre o curso...' onChange={(e) => setText(e.target.value)} value={text}/>
           </label>
           <input type="submit" className='btn' value='Enviar'/>
         </form>
@@ -56,12 +82,12 @@ const addMemory = () => {
       <div className='comments-container'>
         <h3>Comentários ({comments.length})</h3>
         {comments.length === 0 && <p>Não há comentários...</p>}
-        {comments.length > 0 && ((comment) => (
+        {comments.length > 0 && (comments.map((comment) => (
           <div className='comment' key={comment._id}>
             <p className='comment-name'>{comment.name}</p>
             <p className='comment-text'>{comment.text}</p>
           </div>
-        ))}
+        )))}
       </div>
     </div>
   )
